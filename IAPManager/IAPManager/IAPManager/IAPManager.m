92404140876794e23786fc8b78d54efffaa4581f
Valerii Lider <valeriy.lider@gmail.com>
2014-03-25 16:23:57 +0200

    copyright updated

--- IAPManager/IAPManager/IAPManager/IAPManager.m
+++ IAPManager/IAPManager/IAPManager/IAPManager.m
@@ -5,6 +5,12 @@
 //  Created by Valerii Lider on 3/6/14.
 //  Copyright (c) 2014 Spire LLC. All rights reserved.
 //
+//  It is appreciated but not required that you give credit to Valerii Lider,
+//  as the original author of this code. You can give credit in a blog post, a tweet or on
+//  a info page of your app. Also, the original author appreciate letting them know if you use this code.
+//
+//  This code is licensed under the APPACHE license that is available at: http://www.apache.org/licenses/LICENSE-2.0
+//
 
 #import "IAPManager.h"
 
